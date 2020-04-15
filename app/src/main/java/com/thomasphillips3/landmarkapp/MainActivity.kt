fc91cc322a2f7add7b7130e065383bbf801d1288
Thomas Phillips <thomasphillips3@gmail.com>
2020-04-14 22:47:16 -0700

    implement map ui

--- app/src/main/java/com/thomasphillips3/landmarkapp/MainActivity.kt
+++ app/src/main/java/com/thomasphillips3/landmarkapp/MainActivity.kt
@@ -1,12 +1,78 @@
 package com.thomasphillips3.landmarkapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+
+    private var map: GoogleMap? = null
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    private var locationPermissionGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        if (googleMap != null) {
+            map = googleMap
+        }
+        getLocationPermission()
+        updateLocationUi()
+    }
+
+    private fun getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    private fun updateLocationUi() {
+        try {
+            if (locationPermissionGranted) {
+                map?.isMyLocationEnabled = true
+                map?.uiSettings?.isMyLocationButtonEnabled = true
+                map?.uiSettings?.isZoomControlsEnabled = true
+            } else {
+                map?.isMyLocationEnabled = false
+                map?.uiSettings?.isMyLocationButtonEnabled = false
+                map?.uiSettings?.isZoomControlsEnabled = false
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            show(e.message)
+        }
+    }
+
+    private fun show(message: String?) {
+        Log.w(this.localClassName, message)
+        runOnUiThread { Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show() }
+    }
+
+    override fun onMapLongClick(p0: LatLng?) {
+        TODO("Not yet implemented")
     }
 }
