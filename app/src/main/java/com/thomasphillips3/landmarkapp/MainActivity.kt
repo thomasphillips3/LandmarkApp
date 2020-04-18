694e9296d5d62d74b4208f860e09581b8dad5962
Thomas Phillips <thomasphillips3@gmail.com>
2020-04-18 16:38:09 -0700

    show user's current location

--- app/src/main/java/com/thomasphillips3/landmarkapp/MainActivity.kt
+++ app/src/main/java/com/thomasphillips3/landmarkapp/MainActivity.kt
@@ -1,26 +1,37 @@
 package com.thomasphillips3.landmarkapp
 
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private var map: GoogleMap? = null
     private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
     private var locationPermissionGranted = false
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var lastKnownLocation: Location
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -56,11 +67,39 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapLon
                 map?.isMyLocationEnabled = true
                 map?.uiSettings?.isMyLocationButtonEnabled = true
                 map?.uiSettings?.isZoomControlsEnabled = true
+//                map.setOnMapLongClickListener(this)
+                showCurrentLocation()
             } else {
                 map?.isMyLocationEnabled = false
                 map?.uiSettings?.isMyLocationButtonEnabled = false
                 map?.uiSettings?.isZoomControlsEnabled = false
+//                lastKnownLocation = null
                 getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            show(e.message)
+        }
+    }
+
+    private fun showCurrentLocation() {
+        try {
+            if (locationPermissionGranted) {
+                fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+                val location = fusedLocationProviderClient.lastLocation
+                location.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        lastKnownLocation = task.result!!
+                        map?.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude),
+                                15.0F
+                            )
+                        )
+                    } else {
+                        show("Current location is null. Using default.")
+                        map?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
             }
         } catch (e: SecurityException) {
             show(e.message)
